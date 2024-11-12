5334dd01052d30827be4cd5fcd8c3fc1ddcb45dd
paiad <2369538173@qq.com>
2024-11-12 22:26:06 +0800

    feat: deploy 1.1.6 配置笔记页面包屑

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -1,14 +1,14 @@
-import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
+import {defineNoteConfig, defineNotesConfig} from 'vuepress-theme-plume'
 
 const pythonNote = defineNoteConfig({
-  dir: 'python',
-  link: '/python',
-  sidebar: [''],
+    dir: 'python',
+    link: '/python',
+    sidebar: [{text: 'Python', items: ['Deep_and_shallow_copy'], collapsed: true}]
 })
 
 
 export const notes = defineNotesConfig({
-  dir: 'notes',
-  link: '/',
-  notes: [pythonNote],
+    dir: 'notes',
+    link: '/',
+    notes: [pythonNote],
 })
