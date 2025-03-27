1a50723f8abd07af6dd9228707d71720b11218cc
paiad <2369538173@qq.com>
2025-03-27 23:06:01 +0800

    docs: modify config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default defineUserConfig({
     head: [
         // 设置 favor.ico，.vuepress/public 下
         [
-            'link', {rel: 'icon', href: '/favicon.ico'}
+            'link', {rel: 'icon', href: '.vuepress/public/favicon.ico'}
         ],
 
         // 引入自定义字体
